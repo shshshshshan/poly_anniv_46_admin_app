7d3e5eb84a3ad75a6d3db44fd23e1ebb0de8316c
shshshshshan <21-1-01040@vsu.edu.ph>
2024-07-24 13:37:47 +0800

    Formatter utils.

---
+++ lib/utils/formatters/formatter.dart
@@ -0,0 +1,22 @@
+import 'package:intl/intl.dart';
+
+class MainFormatter {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('MMMM d, y').format(date);
+  }
+
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(locale: 'en-PH', symbol: '₱').format(amount);
+  }
+
+  static String formatPhoneNumber(String phoneNumber) {
+    if (phoneNumber.length == 10) {
+      return '${phoneNumber.substring(0, 3)} ${phoneNumber.substring(3, 7)} ${phoneNumber.substring(7)}';
+    } else if (phoneNumber.length == 11) {
+      return '${phoneNumber.substring(0, 4)} ${phoneNumber.substring(4, 8)} ${phoneNumber.substring(8)}';
+    }
+
+    return phoneNumber;
+  }
+}
