ed167854f3c526101113d7792d4b69a916630f15
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-01 18:36:25 +0800

    Create flutter_native_splash options setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:anniv_46_admin_app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
-  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  
+Future<void> main() async {
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  await GetStorage.init();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const App());
 }
