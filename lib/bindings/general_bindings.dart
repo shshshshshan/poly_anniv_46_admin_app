dcb528bf3dc12110ac3cee13e3bf49b300a8db7d
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-02 15:13:50 +0800

    Add network manager deps

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -1,6 +1,9 @@
+import 'package:anniv_46_admin_app/utils/network/network_manager.dart';
 import 'package:get/get.dart';
 
 class GeneralBindings extends Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put(NetworkManager());
+  }
 }
