d83b4a10634bff839b8ead130291e543902652cd
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-02 15:13:03 +0800

    Add login form texts

--- lib/utils/constants/texts.dart
+++ lib/utils/constants/texts.dart
@@ -6,8 +6,23 @@ class MainTexts {
   // -- MAIN Texts
 
   // -- LOGIN Heading Texts
+  static const String loginTitle = 'Welcome back';
+  static const String loginSubTitle =
+      'Track and Monitor Member Attendance in One Click! - Login to Your Account to Get Started!';
 
   // -- LOGIN Form Texts
+  static const String username = 'Username';
+  static const String password = 'Password';
+  static const String rememberMe = 'Remember Me';
+  static const String forgotPassword = 'Forgot Password?';
+  static const String optionalAltLoginText = "Or Sign In With";
+  static const String signIn = 'Sign In';
+  static const String createMyAccount = 'Create My Account';
+  static const String resendEmail = 'Resend Email';
+  static const String resendEmainIn = 'Resend Email in';
+
+  static const String submit = 'Submit';
+  static const String done = 'Done';
 
   // -- DEVICE STORAGE Texts
 }
