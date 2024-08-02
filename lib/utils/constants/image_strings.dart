bb7630160cffd2fbc35aa7b91f18ee501627cc45
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-02 15:13:23 +0800

    Add app logo and google icon

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,9 +1,14 @@
 class MainImages {
   // App Logos
+  static const String lightAppLogo =
+      "assets/logos/polyphonics-splash-logo-light.png";
+  static const String darkAppLogo =
+      "assets/logos/polyphonics-splash-logo-dark.png";
 
   // Email Images
 
   // Animation Images
 
   // Icons
+  static const String googleIcon = "assets/icons/google-icon.png";
 }
