afd3f5a550588a19231f7a2e5b46b88973d87b6a
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-02 15:15:55 +0800

    Initial login header

---
+++ lib/features/authentication/screens/login/widgets/login_header.dart
@@ -0,0 +1,31 @@
+import 'package:anniv_46_admin_app/utils/constants/image_strings.dart';
+import 'package:anniv_46_admin_app/utils/constants/sizes.dart';
+import 'package:anniv_46_admin_app/utils/constants/texts.dart';
+import 'package:anniv_46_admin_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class MainLoginHeader extends StatelessWidget {
+  const MainLoginHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunctions.isDarkMode(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+          height: 150,
+          image: AssetImage(
+              dark ? MainImages.darkAppLogo : MainImages.lightAppLogo),
+        ),
+        const SizedBox(height: MainSizes.sm),
+        Text(MainTexts.loginTitle,
+            style: Theme.of(context).textTheme.headlineMedium),
+        const SizedBox(height: MainSizes.sm),
+        Text(MainTexts.loginSubTitle,
+            style: Theme.of(context).textTheme.bodyMedium),
+      ],
+    );
+  }
+}
