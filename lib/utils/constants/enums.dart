842d7037a91f680a6af8f222eeb6533b62df36e0
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-04 22:41:26 +0800

    Add new enums for registrants

--- lib/utils/constants/enums.dart
+++ lib/utils/constants/enums.dart
@@ -1,3 +1,30 @@
 // LIST OF ENUMS
 
+import 'package:anniv_46_admin_app/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
 enum TextSizes { small, medium, large }
+
+enum Gender { male, female }
+
+extension GenderExtension on Gender {
+  Color get color {
+    return [MainColors.primary, Colors.pink][index];
+  }
+
+  IconData get iconData {
+    return [Icons.boy, Icons.girl][index];
+  }
+}
+
+enum PaymentStatus { pending, successful, failed; }
+
+extension PaymentStatusExtension on PaymentStatus {
+  String get name {
+    return ["Pending", "Successful", "Failed"][index];
+  }
+
+  Color get color {
+    return [MainColors.accent, Colors.green, Colors.red][index];
+  }
+}
