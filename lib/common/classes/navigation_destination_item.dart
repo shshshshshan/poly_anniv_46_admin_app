232a4c053bc922dad1829010bd5778460483714a
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-04 22:47:57 +0800

    Change `name` field to be optional parameter

--- lib/common/classes/navigation_destination_item.dart
+++ lib/common/classes/navigation_destination_item.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NavigationDestinationItem {
-  NavigationDestinationItem(
-      {required this.name, required this.widget, required this.iconData});
+  NavigationDestinationItem({this.name = '', required this.widget, required this.iconData});
 
   final String name;
   final Widget widget;
