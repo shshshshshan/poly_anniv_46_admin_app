e52f0584b1dde72f2f8fbefccc2d07c3ecaa573a
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-06 00:25:27 +0800

    Moved

--- lib/common/classes/registrant.dart
+++
@@ -1,9 +0,0 @@
-import 'package:anniv_46_admin_app/utils/constants/enums.dart';
-
-class Registrant {
-  const Registrant({required this.gender, required this.paymentStatus, required this.name});
-
-  final String name;
-  final Gender gender;
-  final PaymentStatus paymentStatus;
-}
