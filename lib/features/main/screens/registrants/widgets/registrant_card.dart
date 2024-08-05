e83712fb554b2f21a1505650eac45767e4b17606
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-05 16:33:49 +0800

    Refactor registrant to be a class

--- lib/features/main/screens/registrants/widgets/registrant_card.dart
+++ lib/features/main/screens/registrants/widgets/registrant_card.dart
@@ -1,28 +1,27 @@
+import 'package:anniv_46_admin_app/common/classes/registrant.dart';
 import 'package:anniv_46_admin_app/common/widgets/loaders/loaders.dart';
+import 'package:anniv_46_admin_app/features/main/screens/registrants/widgets/registrant_view.dart';
 import 'package:anniv_46_admin_app/utils/constants/colors.dart';
 import 'package:anniv_46_admin_app/utils/constants/enums.dart';
 import 'package:anniv_46_admin_app/utils/constants/sizes.dart';
+import 'package:anniv_46_admin_app/utils/constants/texts.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RegistrantCard extends StatelessWidget {
   const RegistrantCard({
     super.key,
-    required this.gender,
-    required this.name,
-    required this.paymentStatus,
+    required this.registrant,
   });
 
-  final Gender gender;
-  final String name;
-  final PaymentStatus paymentStatus;
+  final Registrant registrant;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () async {
         await Future.delayed(const Duration(milliseconds: 135));
-        Get.to(() => const Scaffold());
+        Get.to(() => RegistrantView(registrant: registrant));
       },
       onLongPress: () => MainLoaders.successSnackbar(
           message: 'Tap for more information.', title: 'Tooltip'),
@@ -31,7 +30,9 @@ class RegistrantCard extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: MainSizes.sm),
           child: Row(
             children: [
-              Expanded(child: Icon(gender.iconData, color: gender.color)),
+              Expanded(
+                  child: Icon(registrant.gender.iconData,
+                      color: registrant.gender.color)),
               Expanded(
                   flex: 4,
                   child: Padding(
@@ -41,25 +42,26 @@ class RegistrantCard extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(name,
+                            Text(registrant.name,
                                 style: Theme.of(context)
                                     .textTheme
                                     .headlineSmall!
-                                    .apply(color: gender.color)),
+                                    .apply(color: registrant.gender.color)),
                             Text.rich(TextSpan(
-                                text: 'Payment Status: ',
+                                text: '${MainTexts.paymentStatus}: ',
                                 style: Theme.of(context)
                                     .textTheme
                                     .labelLarge!
                                     .apply(color: MainColors.darkGrey),
                                 children: [
                                   TextSpan(
-                                      text: paymentStatus.name,
+                                      text: registrant.paymentStatus.name,
                                       style: Theme.of(context)
                                           .textTheme
                                           .labelLarge!
                                           .apply(
-                                              color: paymentStatus.color,
+                                              color: registrant
+                                                  .paymentStatus.color,
                                               fontStyle: FontStyle.italic))
                                 ]))
                           ]))),
