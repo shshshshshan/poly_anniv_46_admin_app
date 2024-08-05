3d9d5ba2a74a9901dbed9797e21f19cadeea1ff6
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-06 00:10:34 +0800

    Enable flutter native splash integration and firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:anniv_46_admin_app/app.dart';
+import 'package:anniv_46_admin_app/data/repositories/authentication_repo.dart';
+import 'package:anniv_46_admin_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 Future<void> main() async {
@@ -9,6 +13,10 @@ Future<void> main() async {
 
   await GetStorage.init();
 
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+    .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
   runApp(const App());
 }
