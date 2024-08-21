6b4247a2ceca1fa757dba279c22ad10948e78b6c
shshshshshan <21-1-01040@vsu.edu.ph>
2024-08-21 22:36:24 +0800

    Add controller for managing payments counter

---
+++ lib/features/main/controllers/dashboard/payment_stats_controller.dart
@@ -0,0 +1,41 @@
+import 'package:anniv_46_admin_app/features/main/controllers/registrants/registrant_controller.dart';
+import 'package:anniv_46_admin_app/features/main/models/registrants/registrant.dart';
+import 'package:anniv_46_admin_app/utils/constants/enums.dart';
+import 'package:get/get.dart';
+
+class PaymentStatsController extends GetxController {
+  static PaymentStatsController get instance => Get.find();
+
+  final pendingPayments = 0.obs;
+  final verifiedPayments = 0.obs;
+  final registrantController = RegistrantController.instance;
+
+  @override
+  void onInit() {
+    super.onInit();
+    updateAll();
+  }
+
+  updateAll() {
+    updatePendingPaymentsCount();
+    updateVerifiedPaymentsCount();
+  }
+
+  updatePendingPaymentsCount() {
+    pendingPayments.value = 0;
+    for (Registrant? registrant in registrantController.registrants) {
+      if (registrant?.paymentStatus == PaymentStatus.pending) {
+        pendingPayments.value++;
+      }
+    }
+  }
+
+  updateVerifiedPaymentsCount() {
+    verifiedPayments.value = 0;
+    for (Registrant? registrant in registrantController.registrants) {
+      if (registrant?.paymentStatus != null && registrant?.paymentStatus != PaymentStatus.pending) {
+        verifiedPayments.value++;
+      }
+    }
+  }
+}
